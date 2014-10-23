b42f401ebf0bfbc7961f8f59c9a292b3e5cd5679
lesiak <lesiak@outlook.com>
2014-10-23 10:28:16 +0200

    Added Nullables project

--- CSharpExamPrep/Enums/Program.cs
+++ CSharpExamPrep/Enums/Program.cs
@@ -10,16 +10,21 @@ namespace Enums {
         Mon, Tue
     }
 
-   
     class Program {
         static void Main(string[] args) {
-        Console.WriteLine(Days.Mon.ToString("d"));
-        Console.WriteLine(Days.Tue.ToString("d"));
+            Console.WriteLine(Days.Mon.ToString("d"));
+            Console.WriteLine(Days.Tue.ToString("d"));
 
-        Console.WriteLine(Days.Mon == 0);
-    
+            Console.WriteLine("Days.Mon == 0: {0}",Days.Mon == 0);
+
+            Days result;
+            //Console.WriteLine("Result before: {0}", result); result is not initialized
+            bool parsed = Enum.TryParse<Days>("BadValue", out result);
+            Console.WriteLine("Parsed?: {0}", parsed);
+            Console.WriteLine("Result: {0}", result);
+
             Console.ReadLine();
         }
-        
+
     }
 }
---
+++ CSharpExamPrep/Nullables/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullables {
+
+    enum MyEnum {
+        A,
+        B
+    }
+
+    class Program {
+        static void Main(string[] args) {            
+
+            Nullable<int> nA = 1;
+            Nullable<int> nB = 1;
+
+            Console.WriteLine("nA.Equals(nB): {0}", nA.Equals(nB));
+            Console.WriteLine("nA.Equals(1): {0}",  nA.Equals(1));
+            Console.WriteLine("nA & nB: {0}",  nA & nB);
+            Console.WriteLine();
+            Console.WriteLine();
+            // Console.WriteLine(nA && nB);
+
+            bool? bA = true;
+            bool? bB = null;
+
+            bool b1 = true;
+            bool b2 = false;
+
+            Console.WriteLine("Bools:");
+
+            Console.WriteLine("bA & bB == null: {0}", (bA & bB) == null);
+            //Console.WriteLine(bA && bB);         //error
+
+            Console.WriteLine("b1 & b2: {0}", b1 & b2);
+            Console.WriteLine("b1 && b2: {0}", b1 && b2);
+
+            //Console.WriteLine(bA && bB);       
+            //stat.aaa(); ERROR, TYPOE Name needed
+
+            Console.ReadLine();
+
+        }
+    }
+}
